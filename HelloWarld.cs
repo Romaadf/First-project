4ff553d687391fd251d3b3988b33efa8a8d652b5
Romaadf <sadikovroma03@gmail.com>
2025-02-22 14:13:10 +0300

    Add files via upload

---
+++ HelloWarld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWarld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("helloWorld");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
